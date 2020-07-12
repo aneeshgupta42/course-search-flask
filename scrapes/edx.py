2f31054e20111f6c6cfcabe408391180c78e8a34
Aneesh Gupta <ag468@duke.edu>
2020-07-12 09:40:26 +0530

    lets test edx

---
+++ scrapes/edx.py
@@ -0,0 +1,74 @@
+import requests 
+from lxml import html
+from urllib.parse import quote
+from bs4 import BeautifulSoup
+
+
+def scrape(course_name):
+    edx_name_parse = quote(course_name)
+    edx_home_url = "https://www.edx.org"
+    edx_url = "https://www.edx.org/search?q=" + edx_name_parse
+    print(edx_url)
+    pageContent=requests.get(edx_url)
+    # print(pageContent.text)
+    soup = BeautifulSoup(pageContent.text, 'html.parser')
+    tree = html.fromstring(pageContent.content)
+    print(tree)
+    records = soup.findAll("div", {"class": "discovery-card-inner-wrapper"})
+    # print(records)
+    # reqd_path = '//li[@class="ais-InfiniteHits-item"]'
+    # records = tree.xpath(reqd_path)
+    print("Num of records on page:", len(records))
+    top_5_records = records[:5]
+    return_list = []
+    for record in top_5_records:
+        # print(str(record))
+        # break
+        data = {}
+        record_soup = BeautifulSoup(str(record), 'html.parser')
+        print(record_soup)
+        
+        try:
+            record_url = record_soup.findAll('a', {"data-click-key":"search.search.click.search_card"})
+            record_link = edx_home_url + record_url[0].get("href")
+            print(record_link)
+            data["link"] = record_link
+        except:
+            data["link"] = edx_url
+
+        try:
+            record_name_span = record_soup.findAll("span", {"class":"partner-name"})
+            record_partner_name = record_name_span[0].text  
+            print(record_partner_name) 
+            data["partner"] = record_partner_name
+        except:
+            data["partner"] = "edx"
+
+        try:
+            record_course_title = record_soup.findAll("h2", {"class":"color-primary-text card-title headline-1-text"})
+            record_title = record_course_title[0].text  
+            print(record_title) 
+            data["title"] = record_title
+        except:
+            data["title"] = course_name
+        
+        try:
+            record_image_div = record_soup.findAll("div", {"class":"image-wrapper"})
+            image = BeautifulSoup(str(record_image_div[0]), 'html.parser').findAll('img')[0]
+            record_image_link = image.get('src')
+            print(record_image_link) 
+            data["image"] = record_image_link
+        except:
+            data["image"] = ""
+        return_list.append(data)
+        print("\n")
+
+    return return_list
+
+    
+
+if __name__ == "__main__":
+    # tree = scrape("computer vision")
+    # tree = scrape("java")
+    tree = scrape("machine learning")
+
